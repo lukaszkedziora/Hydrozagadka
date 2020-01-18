b9110f3153fdfa78fa956a6251f3007ad27d1afd
Łukasz <Kdziora>
2020-01-18 11:47:29 +0100

    better 'if'

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def create_player():
     Creates a 'player' dictionary for storing all player related informations - i.e. player icon, player position.
     Fell free to extend this dictionary!
 
-    Returns:
+    Returns:s
     dictionary
     '''
     pass
@@ -16,14 +16,16 @@ def create_player():
 
 def main(): 
     player = create_player()
-    board = engine.create_board()
-
     util.clear_screen()
     is_running = True
     while is_running:
-        engine.put_player_on_board(board)
-        ui.display_board(board)
+        board = engine.create_board()
+        x = engine.put_player_on_board(board)
+        ui.display_board(x)
+        #print(board, end='')
         key = util.key_pressed()
+        if key == 'w' or 'd' or 's' or 'a':
+            engine.wsad(key)
         if key == 'q':
             is_running = False
         else:
